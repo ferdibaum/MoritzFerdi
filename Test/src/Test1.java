fef1a2b9644260aadccf07cc0b88f883ae177765
Moritz <Moritz@192.168.0.128>
2016-10-20 19:22:02 +0200

    htrhtr

--- Test/src/Test1.java
+++ Test/src/Test1.java
@@ -2,10 +2,7 @@
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("sdfkjsf");
-<<<<<<< HEAD
 		System.out.println("ferdi ist ein scrub");
-=======
 		fdshoijnüfsghiüsfh
->>>>>>> branch 'master' of https://github.com/ferdibaum/MoritzFerdi.git
 	}
 }
