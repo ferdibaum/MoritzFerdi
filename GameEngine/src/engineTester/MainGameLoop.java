4310233a1dd3496874a3fffadbdc52c4e5aff3f9
ferdibaum <ferdi99@live.de>
2016-10-20 23:26:01 +0200

    123

--- GameEngine/src/engineTester/MainGameLoop.java
+++ GameEngine/src/engineTester/MainGameLoop.java
@@ -17,17 +17,18 @@ public class MainGameLoop {
 		Renderer renderer = new Renderer();
 		
 		float[] vertices = { 
-				// Left bottom triangle
 				-0.5f, 0.5f, 0f,
 				-0.5f, -0.5f, 0f,
 				0.5f, -0.5f, 0f,
-				// Right top triangle
-				0.5f, -0.5f, 0f,
 				0.5f, 0.5f, 0f,
-				-0.5f, 0.5f, 0f
 		};
 		
-		RawModel model = loader.loadToVAO(vertices);
+		int[] indices = { 
+				0,1,3,
+				3,1,2
+		};
+		
+		RawModel model = loader.loadToVAO(vertices, indices);
 		
 		while(!Display.isCloseRequested()){
 			renderer.prepare();
