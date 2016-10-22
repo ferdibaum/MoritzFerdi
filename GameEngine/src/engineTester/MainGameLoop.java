58593ddb1cfc5294eb9934fb89a2fc21d7154d18
Moritz <Moritz@192.168.0.128>
2016-10-22 22:49:44 +0200

    123

--- GameEngine/src/engineTester/MainGameLoop.java
+++ GameEngine/src/engineTester/MainGameLoop.java
@@ -31,6 +31,10 @@ public class MainGameLoop {
 		ModelTexture texture = new ModelTexture(loader.loadTexture("Untitled"));
 		
 		TexturedModel staticModel = new TexturedModel(model, texture);
+		//ModelTexture texture = staticModel.getTexture();
+		//texture.setShineDamper(10);
+		//texture.setReflectivity(1);
+		
 		
 		Entity entity = new Entity(staticModel, new Vector3f(0,0,-50),0,0,0,1);
 		Light light = new Light(new Vector3f(0,0,-20),new Vector3f(1,1,1));
