67e9228b62070a84653e429355f9a095b7557a58
Moritz <Moritz@Moritz-PC.fritz.box>
2016-10-23 18:33:44 +0200

    13

--- GameEngine/src/textures/ModelTexture.java
+++ GameEngine/src/textures/ModelTexture.java
@@ -8,11 +8,23 @@ public class ModelTexture {
 	private float reflectivity = 0;
 	
 	private boolean hasTransparency = false;
+	private boolean useFakeLighting = false;
 	
 	public ModelTexture(int id){
 		this.textureID = id;
 	}
 	
+	public boolean isUseFakeLighting() {
+		return useFakeLighting;
+	}
+
+
+	public void setUseFakeLighting(boolean useFakeLighting) {
+		this.useFakeLighting = useFakeLighting;
+	}
+
+
+
 	public boolean isHasTransparency() {
 		return hasTransparency;
 	}
