0866268c02bdf2ab958ddfa17011b0c3c8f46cba
Moritz <Moritz@192.168.0.128>
2016-10-24 00:03:32 +0200

    123

--- GameEngine/src/entities/Camera.java
+++ GameEngine/src/entities/Camera.java
@@ -32,7 +32,8 @@ public class Camera {
 			position.z = position.z + diff.z * -0.2f;
 		}
 		if(Keyboard.isKeyDown(Keyboard.KEY_D)){
-			position.x+=0.2f;
+			position.x = position.x + diff.x * 0.2f;
+			position.z = position.z + diff.z * -0.2f;
 		}
 		if(Keyboard.isKeyDown(Keyboard.KEY_A)){
 			position.x-=0.2f;
