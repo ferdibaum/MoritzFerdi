d1fa1176f6834a3cbb8e8d025f871b941546847d
Moritz <Moritz@192.168.0.128>
2016-10-24 00:10:49 +0200

    13

--- GameEngine/src/entities/Camera.java
+++ GameEngine/src/entities/Camera.java
@@ -12,19 +12,24 @@ public class Camera {
 	
 	public Camera(){}
 	
+	public static double getSin(double angleInDegrees) {
+		 double angleInRadians = Math.toRadians(angleInDegrees);
+		 return Math.sin(angleInRadians);
+		}
+	
+	public static double getCos(double angleInDegrees) {
+		 double angleInRadians = Math.toRadians(angleInDegrees);
+		 return Math.cos(angleInRadians);
+		}
+	
 	public void move(){
+		Vector3f diff = new Vector3f();
+		diff.x = (float) (getSin((double)yaw));
+		diff.y = 0; 
+		diff.z = (float) (getCos((double)yaw));
 		if(Keyboard.isKeyDown(Keyboard.KEY_W)){
-<<<<<<< HEAD
 			position.x = position.x + diff.x * 0.2f;
 			position.z = position.z + diff.z * -0.2f;
-=======
-			Vector3f diff = new Vector3f();
-			diff.z = -0.2f;
-			position.x = position.x + diff.x;
-			position.y = position.y + diff.y;
-			position.z = position.z + diff.z;
-
->>>>>>> refs/remotes/origin/master
 		}
 		if(Keyboard.isKeyDown(Keyboard.KEY_D)){
 			position.x = position.x + diff.x * 0.2f;
@@ -43,6 +48,7 @@ public class Camera {
 		if(Keyboard.isKeyDown(Keyboard.KEY_Q)){
 			yaw-=0.2f;
 		}
+		System.out.println(yaw + "\t" + diff.x + "\t" + diff.z);
 	}
 
 	public Vector3f getPosition() {
