bf541eb5720140767e266851dfda36c370ecd235
ferdibaum <ferdi99@live.de>
2017-02-12 18:47:53 +0100

    4687ue6tzjuydn

--- GameEngine/src/renderEngine/DisplayManager.java
+++ GameEngine/src/renderEngine/DisplayManager.java
@@ -11,8 +11,8 @@ import org.lwjgl.opengl.PixelFormat;
 
 public class DisplayManager {
 
-	public static final int WIDTH = 1920; //1280;
-	public static final int HEIGHT = 1080; //720;
+	public static final int WIDTH = 1280; //1280;
+	public static final int HEIGHT = 720; //720;
 	public static final int FPS_CAP = 130;
 	
 	private static long lastFrameTime;
